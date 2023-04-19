0e6bf256db8b54dfe26a7a6d0dc5e233e4acc8c3
jesus e <jesisp17@gmail.com>
2023-04-19 20:35:52 -0300

    added remove from db function

--- src/firebase/db/dbFunctions.js
+++ src/firebase/db/dbFunctions.js
@@ -2,7 +2,6 @@ import { database } from "../config";
 import { ref, set, get, child, remove } from "firebase/database";
 
 export const postEventToDb = async({ title, description, startTime, endTime, _id, user} ) => {
-    debugger
     try {
         await set(ref(database, `events/${user.id}/${_id}`), {
           title,
@@ -16,6 +15,16 @@ export const postEventToDb = async({ title, description, startTime, endTime, _id
     } catch (error) {
         console.error(error)
         return new Error( Error )
+    }
+}
+
+export const removeEventFromDb = async(id) => {
+    try{
+        await remove(ref(database, `events/${id}`))
+        console.log('Event deleted')
+    }catch(error){
+        console.error(error)
+        return new Error(Error) 
     }
 }
 
